08b7963fdc5a00ceed2505b5983217d7f75a26a3
Albertoherrerosmag <61705874+AlbertoLimas@users.noreply.github.com>
2020-04-30 19:34:53 +0200

    Create Constructor_copia.h

---
+++ Constructor_copia.h
@@ -0,0 +1,28 @@
+#ifndef GARITO_H
+#define GARITO_H
+
+class Garito {
+public:
+    
+    Garito()=default;
+    Garito(const Garito& orig);
+
+private:
+    string _nombre="";
+    string _direccion="";
+};
+
+#endif /* GARITO_H */
+
+//Garito.cpp
+
+#include "Garito.h"
+
+Garito::Garito(const Garito& orig):
+_nombre(orig._nombre),_direccion(orig._direccion){
+}
+
+//main.cpp
+
+Garito* g1 = new Garito();
+Garito* g2 = g1; ///< Uso del constructor de copia, el objeto g2 será igual que el objeto g1
