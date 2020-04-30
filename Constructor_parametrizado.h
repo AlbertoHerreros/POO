8bfb0e5834e2d0ae882090f583b983cfe83b3f04
Albertoherrerosmag <61705874+AlbertoLimas@users.noreply.github.com>
2020-04-30 19:30:46 +0200

    Create Constructor_parametrizado.h

---
+++ Constructor_parametrizado.h
@@ -0,0 +1,37 @@
+#ifndef GARITO_H
+#define GARITO_H
+
+class Garito {
+public:
+ 
+    Garito(string nombre, string direccion);
+    
+private:
+    string _nombre="";
+    string _direccion="";
+};
+
+#endif /* GARITO_H */
+
+//Garito.cpp
+
+#include "Garito.h"
+
+Garito::Garito(nombre,direccion):
+_nombre(nombre),_direccion(direccion){
+}
+///----------------------------------------------------------------------------------------------------------------------------------------
+//SEGUNDO MÉTODO PARA CONSTRUCTOR PARAMETRIZADO
+class Garito {
+public:
+ 
+    Garito(string nombre, string direccion="Direccion");  ///< Asignar los valores directamente en el constructor, deberán 
+                                                          ///< ponerse de derecha a izquierda
+private:
+    string _nombre="";
+    string _direccion="";
+};
+
+//main.cpp
+
+Garito* g= new Garito("NombreGarito"); ///< Creará un garito ("NombreGarito","Direccion");
