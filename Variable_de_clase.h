077028911b0a74ab13b12949b3172aeea0d52d0b
Albertoherrerosmag <61705874+AlbertoLimas@users.noreply.github.com>
2020-04-30 19:50:45 +0200

    Create Variable_de_clase.h

---
+++ Variable_de_clase.h
@@ -0,0 +1,26 @@
+static <tipo> dato=;
+
+variable de clase que siempre tendrá el valor que se le vaya asignando
+
+//.h
+public:
+  static int _numTemazos;
+------------------------------------------------------------------------------------------------------------------------------------
+//.cpp
+
+int Temazo::_numTemazos = 0;
+
+Temazo::Temazo(){
+     _titulo="";
+     _interprete="";
+     _duracion=0;
+     _puntuacion=0;
+     _nombreUltimoGarito="";
+     _fechaUltimoUso=Fecha();
+     _idTemazo=numTemazos;
+    numTemazos++;   
+}
+-------------------------------------------------------------------------------------------------------------------------------------
+Ejemplo: numTemazos es una variable de clase que contará el nº que determinará el orden de creación, asi como puede ser usado
+para identificar temazos
+
