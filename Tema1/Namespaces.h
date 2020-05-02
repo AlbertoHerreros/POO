a9e8affaebb3a2621a580eac5c5046cde81a82eb
Albertoherrerosmag <61705874+AlbertoLimas@users.noreply.github.com>
2020-05-02 12:00:10 +0200

    Create Namespaces.h

---
+++ Tema1/Namespaces.h
@@ -0,0 +1,21 @@
+#ifndef DJUTILS_H
+#define	DJUTILS_H
+
+namespace djutils{
+  
+  void funcion(<Clase>& clase);
+  
+}
+
+#endif	/* DJUTILS_H */
+
+//.h
+
+void djutils::funcion(<Clase>& clase){
+  ///< Código que realiza
+}
+
+//main.cpp
+
+#include "djutils.h"
+funcion(objeto tipo <Clase>);
