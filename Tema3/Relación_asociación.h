681ae85fb78cdd42e1bc53949e03c8e9ac2b8994
Albertoherrerosmag <61705874+AlbertoLimas@users.noreply.github.com>
2020-05-02 12:58:22 +0200

    Update Relación_asociación.h

--- Tema3/Relación_asociación.h
+++ Tema3/Relación_asociación.h
@@ -1,5 +1,33 @@
 //Ojetos de la clase A trabajan de forma continuada con objetos de la clase B
 
+#include "Clase2.h"
+
+class Clase1
+{
+  private:
+    std::string _nombre;
+    Clase2* <rol> = nullptr; ///< Atributo que establece la relación
+}
+
+//.cpp
+/*
+  Una buena práctica es introducir en el destructor de clase una excepción lanzada por cerr para evitar errores con excepciones 
+  en destructores que determinará si se le ha quitado o no la asociación de un objeto <Clase2> a <Clase1>.
+  Si no, tendríamos un objeto <Clase2> que quedaría perdido.
+  EL DESTRUCTOR NO DESTRUYE LOS OBJETOS ASOCIADOS
+*/
+  Clase1::~Clase1(){
+    if( <rol> != nullptr ){     ///< ¿Tiene objeto asociado?
+      cerr << "No se le ha quitado el objeto <Clase2> a objeto <Clase1>";
+    }
+  }
+
+  Clase2* Clase1::quitar(){
+    Clase2 aux = <rol>;             ///< Destrucción de objeto asociado
+    <rol> = nullptr;
+    return aux;
+  }
+
 /*
 UML:
   -Nombre de asociación: "hecho por, escrito por..."
