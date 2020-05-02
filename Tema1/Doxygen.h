8709d9bbae1f72f4447fd5f4ea3ed8b94a432506
Albertoherrerosmag <61705874+AlbertoLimas@users.noreply.github.com>
2020-05-02 12:21:28 +0200

    Create Doxygen.h

---
+++ Tema1/Doxygen.h
@@ -0,0 +1,10 @@
+@brief Resumen de la función implementada
+
+@param <nombreParámetro> Parámetro de la función
+
+@return Qué devuelve la función
+
+@pre Precondición necesaria
+@post Postcondición
+
+@file @autor @Class
