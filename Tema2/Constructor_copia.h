892eb8f225fced616ba9105eb57346da6e414357
Albertoherrerosmag <61705874+AlbertoLimas@users.noreply.github.com>
2020-05-17 18:15:54 +0200

    Update Constructor_copia.h

--- Tema2/Constructor_copia.h
+++ Tema2/Constructor_copia.h
@@ -24,5 +24,9 @@ _nombre(orig._nombre),_direccion(orig._direccion){
 
 //main.cpp
 
+Garito g1;
+Garito g2 (g1);
+Garito g2=g1; ///< Si no existen operadores sobrecargados
+
 Garito* g1 = new Garito();
 Garito* g2 = g1; ///< Uso del constructor de copia, el objeto g2 será igual que el objeto g1
