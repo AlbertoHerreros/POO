64a8d98ca4fdb528ce515069b6e9efb8e10a5558
Albertoherrerosmag <61705874+AlbertoLimas@users.noreply.github.com>
2020-05-18 12:59:30 +0200

    Create Plantilla_clase.h

---
+++ Tema4/Plantilla_clase.h
@@ -0,0 +1,6 @@
+template<typename T>
+tipo <nombre>{
+//FUNCIÓN QUE USES
+}
+
+//Si usas esta plantilla para ordenar objetos, es necesario que tengas en cuenta sobrecargar los operadores para evitar fallos
