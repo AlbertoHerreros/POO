cfea1e0663cce855c2a4f4ce8b54f1aa3902e57d
Albertoherrerosmag <61705874+AlbertoLimas@users.noreply.github.com>
2020-06-22 01:00:18 +0200

    Update Punteros.h

--- Tema2/Punteros.h
+++ Tema2/Punteros.h
@@ -1,5 +1,12 @@
-Clase v[10]; //Inicializados con constructor por defecto
-v[0] = Clase();
-
+Clase w[10]; //Inicializados con constructor por defecto
+w[0] = Clase();
+///---------------------------------------------------------
 Clase* v[10]; //No inicializados
 v[0] = new Clase();
+
+for(int i=0;i<10;i++){
+        if( v[i]!=nullptr ){
+            delete v[i];
+            v[i]=nullptr;
+        }
+    }
