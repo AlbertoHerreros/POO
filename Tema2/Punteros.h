3451a0f717e103eba37bca89aa7682ce0fc93e06
Albertoherrerosmag <61705874+AlbertoLimas@users.noreply.github.com>
2020-06-22 13:39:30 +0200

    Update Punteros.h

--- Tema2/Punteros.h
+++ Tema2/Punteros.h
@@ -1,8 +1,12 @@
-Clase w[10]; //Inicializados con constructor por defecto
+Clase w[10]; //Inicializados con constructor por defecto, los 10 objetos tienen valores por defecto
 w[0] = Clase();
 ///---------------------------------------------------------
-Clase* v[10]; //No inicializados
+Clase* v[10]; //No inicializados, punteros vacíos
 v[0] = new Clase();
+
+for(int i=0;i<10;i++){
+        v[i]=new Clase();//Inicializar vector dinámico
+}
 
 for(int i=0;i<10;i++){
         if( v[i]!=nullptr ){
