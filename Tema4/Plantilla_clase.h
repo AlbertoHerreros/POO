172f07b8bcd8514e67d5190d885ea7ebca7bfe28
Albertoherrerosmag <61705874+AlbertoLimas@users.noreply.github.com>
2020-10-10 18:43:27 +0200

    Update Plantilla_clase.h

--- Tema4/Plantilla_clase.h
+++ Tema4/Plantilla_clase.h
@@ -1,6 +1,17 @@
-template<typename T>
-tipo <nombre>{
+template<class T>
+class <nombreClase>{
 //FUNCIÓN QUE USES
+  void funcion();
 }
 
+
+
+template<class T>
+void nombreClase<T>::funcion(){
+  //Código
+}
+template<class T>
+  //Siempre que declares debajo cada método es necesario escribir template<class T>
+
 //Si usas esta plantilla para ordenar objetos, es necesario que tengas en cuenta sobrecargar los operadores para evitar fallos
+//.cpp no existe -> IMPLEMENTACIÓN EN CABECERA
