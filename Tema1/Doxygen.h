76399f8b1dd26a14168f0a2d5d86a0836905ce5b
Albertoherrerosmag <61705874+AlbertoHerreros@users.noreply.github.com>
2020-12-02 22:34:17 +0100

    Update Doxygen.h

--- Tema1/Doxygen.h
+++ Tema1/Doxygen.h
@@ -1,10 +1,11 @@
 @brief Resumen de la función implementada
 
-@param <nombreParámetro> Parámetro de la función
+@param <nombreParámetro>: Parámetro de la función, detallar de qué se trata
 
-@return Qué devuelve la función
+@return En caso de qué la función devuelva algún valor, especificarlo
 
-@pre Precondición necesaria
+@pre Precondición necesaria para la ejecución de la función
+
 @post Postcondición
 
 @file @autor @Class
