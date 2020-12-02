3bfb8d499d3bbc2dfc9ddffd1c3be2220610c825
Albertoherrerosmag <61705874+AlbertoHerreros@users.noreply.github.com>
2020-12-02 23:54:01 +0100

    Update Variable_de_clase.h

--- Tema2/Variable_de_clase.h
+++ Tema2/Variable_de_clase.h
@@ -5,23 +5,23 @@ variable de clase que siempre tendrá el valor que se le vaya asignando
 
 //.h
 public:
-  static int _numTemazos;
+  static int _numObjetos;
 ------------------------------------------------------------------------------------------------------------------------------------
 //.cpp
 
-int Temazo::_numTemazos = 0;
+int Clase::_numObjetos = 0;
 
-Temazo::Temazo(){
+Clase::Clase(){
      _titulo="";
      _interprete="";
      _duracion=0;
      _puntuacion=0;
      _nombreUltimoGarito="";
      _fechaUltimoUso=Fecha();
-     _idTemazo=_numTemazos;
-    _numTemazos++;   
+     _idObjeto = _numObjetos; //La id del objeto será su posición -> _numObjetos del momento
+    _numObjetos++;   //_numObjetos++ porque crece en 1 el nº de objetos del mismo tipo
 }
 -------------------------------------------------------------------------------------------------------------------------------------
-Ejemplo: _numTemazos es una variable de clase que contará el nº que determinará el orden de creación, asi como puede ser usado
-para identificar temazos
+Ejemplo: _numObjetos es una variable de clase que contará el nº que determinará el orden de creación, asi como puede ser usado
+para identificar Objetos
 
