18036f2465c3e3526b27b9871952a84f090d8143
Albertoherrerosmag <61705874+AlbertoHerreros@users.noreply.github.com>
2020-12-02 23:51:18 +0100

    Update StringStream.h

--- Tema2/StringStream.h
+++ Tema2/StringStream.h
@@ -2,21 +2,21 @@
 
 //.h
 
- std::string toCSV(); ///< Función que devuelve los datos de una clase en formato CSV
+ std::string toCSV(); //Función que devuelve los datos de una clase en formato CSV
 
 //.cpp
 
 std::string Clase::toCSV(){
   std::stringstream ss;
   ss << <var1> ";" << <var2> << ";" << <varx> ;
-  return ss.str();
+  return ss.str(); //Se retorna la concatenación de var1;var2;varx
 }
 
 -------------------------------------------------------------------------------------------------------------------------------------------
 
 //.h
 
-void fromCSV( std::string& datos );      ///< Función que asigna los datos de un objeto a través de un texto en formato CSV
+void fromCSV( std::string& datos );      //Función que asigna los datos de un objeto a través de un texto en formato CSV
 
 //.cpp
 
@@ -26,10 +26,10 @@ void Clase::fromCSV( std::string& datos)
 
    aux << datos;
 
-   getline ( aux, this-><var1string>, ';' ); ///< Así ignoramos en la entrada de palabras el ";"
+   getline ( aux, this-><var1string>, ';' ); //Así ignoramos en la entrada de palabras el ";"
    getline ( aux, this-><var2string>, ';' );
    aux >> this-><var3num>;
-   aux.ignore (1);      ///< En la entrada de números ignoramos un espacio para saltar el ";"
+   aux.ignore (1);      //En la entrada de números ignoramos un espacio para saltar el ";"
    aux >> this-><var4num>;
    aux.ignore (1);
    getline ( aux, this->_incidenciasUltimaMision );
