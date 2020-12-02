0ef155789d7b81b470b37090724ad3691f55b586
Albertoherrerosmag <61705874+AlbertoHerreros@users.noreply.github.com>
2020-12-02 23:17:50 +0100

    Update Excepciones_con_clase.h

--- Tema2/Excepciones_con_clase.h
+++ Tema2/Excepciones_con_clase.h
@@ -3,37 +3,57 @@
 class ParametroNoValido {
 public:
     
-    ParametroNoValido()=default;
-    ParametroNoValido(string descripcion, string funcion="", string fichero="");
-    ParametroNoValido(const ParametroNoValido& orig);
+    ParametroNoValido() = default;
+    ParametroNoValido( string descripcion, string funcion = "", string fichero = "" );
+    ParametroNoValido( const ParametroNoValido& orig );
     virtual ~ParametroNoValido();
     
     string queOcurre( string fichero,  string funcion,  string descripcion);
     
 private:
-    string _fichero="";
-    string _funcion="";
-    string _descripcion="";
+    string _fichero = "";
+    string _funcion = "";
+    string _descripcion = "";
 };
 
---------------------------------------------------------------------------------------------------
+//ParametroNoValido.cpp
+
+ParametroNoValido::ParametroNoValido( string fichero, string funcion, string descripcion ):
+_fichero(fichero),_funcion(funcion),_descripcion(descripcion){
+
+}
+
+ParametroNoValido::ParametroNoValido( const ParametroNoValido& orig ):
+_fichero(orig._fichero),_funcion(orig._funcion),_descripcion(orig._descripcion){
+}
+
+ParametroNoValido::~ParametroNoValido() {
+}
+
+string ParametroNoValido::queOcurre() {
+    std::stringstream ss;
+    ss << _fichero << ";" << _funcion << ";" << _descripcion ;
+    return ss.str();
+}
+
+//--------------------------------------------------------------------------------------------------
 //.cpp de otra clase en la que queramos usar la excepcion
 #include "ParametroNoValido.h"
 
-void Garito::SetDireccion(string direccion){
-    if(direccion==""){
-        throw ParametroNoValido("Garito.h",”Garito::SetDirección”,”descripcion”); ///< Lanza una excepción de la Clase ParámetroNoValido
+void Clase::SetDireccion( string direccion ){
+    if( direccion == "" ){
+        throw ParametroNoValido( "Clase.h",”Clase::SetDirección”,”descripcion” ); //Lanza una excepción de la Clase ParámetroNoValido
     }
     else{
-        this->_direccion=direccion;
+        this->_direccion = direccion;
     }
 }
 
 //main.cpp
-
+#include "ParametroNoValido.h"
 try{
 	//Código a realizar
 
-}catch (ParametroNoValido &e){
-	cerr << " " << e.get[variable con la descripcion o variable que concatene los valores/texto a mostrar]() << std::endl;
+}catch (ParametroNoValido &e){ //Si existiera excepción alguna se lanzaría al terminar la ejecución
+	cerr << " " << e.get[variable con la descripción o variable que concatene los valores/texto a mostrar]() << std::endl;
 }
