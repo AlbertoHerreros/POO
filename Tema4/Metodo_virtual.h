1ff3b805bd30f9cc23bbfcdc1d8004ff0c051219
Albertoherrerosmag <61705874+AlbertoHerreros@users.noreply.github.com>
2021-02-11 22:57:27 +0100

    Update Metodo_virtual.h

--- Tema4/Metodo_virtual.h
+++ Tema4/Metodo_virtual.h
@@ -11,3 +11,5 @@ class B: public A{
 }
 
 //Dependiendo del tipo de objeto nos devolverá un valor u otro
+//Si pedimos a un objeto estático B que nos diga qué es, dirá que es un objeto de la clase A
+//En cambio si el objeto es dinámico, nos dirá que pertenece a la clase B
